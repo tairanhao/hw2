da68e9dfaad81b0dfcd65775ac2554c066e75450
tairanhao <55215285+tairanhao@users.noreply.github.com>
2019-09-18 22:55:09 -0700

    Create updown.js

---
+++ updown.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+var ballX = 30;
+var ballSpeed = 3;
+
+var ballX2 = 370;
+var ballSpeed2 = 5;
+
+function draw() {
+  background(220);
+
+  fill(255, 0, 0);
+  ellipse(110, ballX, 30);
+  
+  fill(0, 0, 255);
+  ellipse(200, ballX2, 30);
+
+  ballX = ballX + ballSpeed;
+  ballX2 = ballX2 + ballSpeed2;
+
+  if (ballX >= 400) {
+    ballSpeed = -3;
+  }
+
+  if (ballX2 >= 400) {
+    ballSpeed2 = -5;
+  }
+
+  if (ballX <= 0) {
+    ballSpeed = 3;
+  }
+
+  if (ballX2 <= 0) {
+    ballSpeed2 = 5;
+  }
+
+}
